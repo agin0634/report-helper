cfe9d7ac81497305d94ac591630ec7c87ab99691
Duri Shen <agin0511@gmail.com>
2020-07-08 18:00:20 +0800

    add image search, report export

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 if hasattr(sys, 'frozen'):
     os.environ['PATH'] = sys._MEIPASS + ";" + os.environ['PATH']
 from openpyxl import load_workbook
+from openpyxl import Workbook
 from mainUI import Ui_MainWindow
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow
@@ -12,50 +13,105 @@ from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow
 class App():
     def readFile(self,file):
         if file.endswith(".xlsx"):
-            self.wbRef = load_workbook(file)
+            try:
+                self.wbRef = load_workbook(file)
+            except:
+                print("Please import valid source file.")
         else:
             print("This file format is not supported. (.xlsx)")
 
-    def writeFile(self):
-        #wb = xlsxwriter.Workbook('report.xlsx')
-        wsRef = self.wbRef.get_sheet_by_name(self.wbRef.sheetnames[0])
-        content = self.loadSheetContent(self,wsRef)
-        print(content, end=" ")
-        
-        '''
-        try:
-            wb = xlsxwriter.Workbook('report.xlsx')
-            self.xlsHelper(self,wb)
-        except:
-            print("File being used by another process, please close the process and try again.")
-            pass
-        '''
+    def Search(self,path):
+        if self.wbRef:
+            if path:
+                try:
+                    wsRef = self.wbRef.get_sheet_by_name(self.wbRef.sheetnames[0])
+                    self.soldImgNames = []
+                    self.unsoldcontent = []
+                    self.content, self.soldImgNames = self.loadSheetContent(self,wsRef)
+                    self.findImage(self,path)
+                    self.bIsSearchComplete = True
+                    print("Search complete.")
+                except:
+                    print("Search ERROR.")
+            else:
+                print("Please select a images path.")
+        else:
+            print("Please select a source path.")
         
     def loadSheetContent(self,ws):
         rows = ws.rows
         columns = ws.columns
-        content = []
+        content, imgNames = [], []
         for row in rows:
-            line = [col.value for col in row]
+            line = []
+            imgNames.append(row[0].value)
+            for col in row:
+                line.append(col.value)
             content.append(line)
-        return content
+        return content, imgNames
         
-    # def foundImage(self):
-    # def write2Sheet(self):
-    '''
-        ws = wb.add_worksheet()
-        # Headline
-        ws.write('A2', '列標籤')
-        ws.set_column('A:A', 10)
-        ws.write('B2', '加總 - Total Order Items')
-        ws.set_column('B:B', 24)
-        ws.write('C2', 'ARTWORK')
-        ws.set_column('C:C', 12)
-        # 
-        #print(self.wsRef.cell('A2').value)
+    def findImage(self,path):
+        for dirPath, dirNames, fileNames in os.walk(path):
+            for name in fileNames:
+                if name.endswith('.png') or name.endswith('.jpg'):
+                    path = os.path.join(path, dirPath, name)
+                    base = os.path.splitext(name)[0]
+                    bIsVaildName = False
+                    for i in range(len(self.soldImgNames)):
+                        if base == self.soldImgNames[i]:
+                            self.content[i][2] = path
+                            bIsVaildName = True
+                            
+                    if bIsVaildName == False:
+                        self.unsoldcontent.append([base,path])
+        
+    def exportSoldReport(self):
+        if self.bIsSearchComplete:
+            try:
+                wb = xlsxwriter.Workbook('Sold Report.xlsx')
+                ws = wb.add_worksheet()
+                ws.set_column('B:B', 24)
+                ws.set_column('C:C', 18.43)
+                for i in range(len(self.content)):
+                    index = 0
+                    for j in range(len(self.content[i])-1):
+                        ws.write(i,j,self.content[i][j])
+                        index += 1
+                    if self.content[i][index] != None:
+                        if self.content[i][index].endswith('.png') or self.content[i][index].endswith('.jpg'):
+                            ws.set_row(i,102)
+                            ws.insert_image(i,index,self.content[i][index], {'x_scale':0.075, 'y_scale':0.075, 'object_position':3})
+                        else:
+                            ws.write(i,index,self.content[i][index])
+                    else:
+                        ws.write(i,index,self.content[i][index])
+                wb.close()
+                print("Sold report export complete.")
+            except:
+                print("Sold report export ERROR.")
+        else:
+            print("Please search images first.")
 
-        wb.close()
-        '''
+    def exportUnsoldReport(self):
+        if self.bIsSearchComplete:
+            try:
+                wb = xlsxwriter.Workbook('Unsold Report.xlsx')
+                ws = wb.add_worksheet()
+                ws.set_column('B:B', 18.43)
+                for i in range(len(self.unsoldcontent)):
+                    index = 0
+                    for j in range(len(self.unsoldcontent[i])-1):
+                        ws.write(i,j,self.unsoldcontent[i][j])
+                        index += 1
+                    if self.unsoldcontent[i][index].endswith('.png') or self.unsoldcontent[i][index].endswith('.jpg'):
+                        ws.set_row(i,102)
+                        ws.insert_image(i,index,self.unsoldcontent[i][index], {'x_scale':0.075, 'y_scale':0.075, 'object_position':3})
+                wb.close()
+                print("Unsold report export complete.")
+            except:
+                print("Unsold report export ERROR.")
+        else:
+            print("Please search images first.")
     
 class AppWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -64,18 +120,40 @@ class AppWindow(QMainWindow, Ui_MainWindow):
         self.ui.setupUi(self)
 
     def eventExcelBrowseData(self):
-        data = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File',os.getcwd(),"All Files(*xlsx)")
-        if data:
-            self.ui.excellineEdit.setText(data[0])
-            App.readFile(App,self.ui.excellineEdit.text())
-            App.writeFile(App)
+        try:
+            data = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File',os.getcwd(),"All Files(*xlsx)")
+            if data:
+                self.ui.excellineEdit.setText(data[0])
+                App.readFile(App,self.ui.excellineEdit.text())
+                App.writeFile(App)
+        except:
+            pass
     
     def eventImageBrowseData(self):
-        data = QtWidgets.QFileDialog.getExistingDirectory(None, 'Open Dir',os.getcwd())
-        if data:
-            self.ui.imagelineEdit.setText(data)
+        try:
+            data = QtWidgets.QFileDialog.getExistingDirectory(None, 'Open Dir',os.getcwd())
+            newData = ""
+            if data:
+                for c in data:
+                    if c == "/":
+                        c = "\\"
+                    newData += c
+                self.ui.imagelineEdit.setText(newData)
+        except:
+            pass
+
+    def eventSearch(self):
+        App.Search(App,self.ui.imagelineEdit.text())
+    
+    def eventExportSoldReport(self):
+        App.exportSoldReport(App)
+
+    def eventExportUnsoldReport(self):
+        App.exportUnsoldReport(App)
 
 if __name__ == "__main__":
+    App.wbRef = None
+    App.bIsSearchComplete = False
     app = QApplication(sys.argv)
     win = AppWindow()
     win.show()
